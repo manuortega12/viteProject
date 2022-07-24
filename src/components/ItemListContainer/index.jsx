8508f6565961a93bc9f2aca3158a18f0ca885525
manuortega12 <manuortega12@gmail.com>
2022-07-24 19:24:13 -0300

    boton itemcount

--- src/components/ItemListContainer/index.jsx
+++ src/components/ItemListContainer/index.jsx
@@ -1,10 +1,19 @@
+import ItemCount from "../ItemCount";
 import React from "react";
 import Title from "../Title";
 
 
 const ItemListContainer = ({ texto }) => {
+
+    const onAdd = (quantity) => {
+        console.log(`Compraste ${quantity} unidades`);
+    }
+
     return (
-        <Title greeting={texto}/>
+        <>
+            <Title greeting={texto}/>
+            <ItemCount initial={1} stock={5} onAdd={onAdd}/>
+        </>
     )
 }
 
