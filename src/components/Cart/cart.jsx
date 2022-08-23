8f3ca1924acd1c90966e645f4668e0bedb0354a7
manuortega12 <manuortega12@gmail.com>
2022-08-23 19:02:00 -0300

    Item Collection II

--- src/components/Cart/cart.jsx
+++ src/components/Cart/cart.jsx
@@ -1,3 +1,4 @@
+import { addDoc, collection, getFirestore } from 'firebase/firestore';
 import React from 'react'
 import { Link } from 'react-router-dom';
 import { useCartContext } from '../context/CartContext'
@@ -5,6 +6,24 @@ import ItemCart from '../ItemCart/itemCart';
 
 const Cart = () => {
     const { cart, totalPrice } = useCartContext();
+
+    const order = {
+        buyer: {
+            name: 'Manu',
+            email: 'manu@manu.com',
+            phone: '1234564',
+            adrress: 'asdadad'
+        },
+        items: cart.map(product => ({ id: product.id, title: product.title, price: product.price, quantity: product.quantity })),
+        total: totalPrice(),
+    }
+
+    const handleClick = () => {
+        const db = getFirestore();
+        const ordersCollection = collection(db, 'orders');
+        addDoc(ordersCollection, order)
+        .then(( {id} ) => console.log(id));
+    }
 
     if (cart.length === 0) {
         return (
@@ -23,6 +42,7 @@ const Cart = () => {
             <p>
                 Total: {totalPrice()}
             </p>
+            <button onClick={handleClick}>Realizar compra</button>
         </>
     )    
 }
